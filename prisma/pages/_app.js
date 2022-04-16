af78dd91c09c249d4520d5d61f3430998d83ae1f
thevvalverde <fvalverde007@gmail.com>
2022-04-16 13:58:51 +0100

    Finished interaction

--- prisma/pages/_app.js
+++ prisma/pages/_app.js
@@ -1,8 +1,51 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import '../styles/styles.css'
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: "#e5ffff",
+            main: "#b2ebf2",
+            dark: "#81b9bf",
+            contrastText: "#000000",
+        },
+        secondary: {
+            light: "#454c6a",
+            main: "#1b243f",
+            dark: "#00001a",
+            contrastText: "#ffffff",
+        },
+        error: {
+            light: "#d55b4e",
+            main: "#9e2b25",
+            dark: "#690000",
+            contrastText: "#ffffff",
+        },
+        warning: {
+            light: "#fffb88",
+            main: "#ffc857",
+            dark: "#c89825",
+            contrastText: "#000000",
+        },
+        info: {
+            light: "#e4bfff",
+            main: "#b18fcf",
+            dark: "#81619e",
+            contrastText: "#000000",
+        },
+        success: {
+            light: "#d8ff9b",
+            main: "#a5cc6b",
+            dark: "#749b3d",
+            contrastText: "#000000",
+        }
+    }
+})
+
 export default function App({Component, pageProps}) {
-
-    console.log("PAGEPROPS: " + pageProps);
-
-    return <Component {...pageProps} />
+    return (
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider> 
+            )
 }
--- prisma/pages/index.jsx
+++ prisma/pages/index.jsx
@@ -1,115 +1,55 @@
-import { Button, Typography } from "@mui/material"
-import { prisma } from "@prisma/client"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import LaunchIcon from '@mui/icons-material/Launch'
-import { ExpandMore } from "@mui/icons-material";
-import { useFormik } from "formik"
-import Consent from "../components/Consent"
+import { Button, Container, Paper, TextField, Typography } from "@mui/material"
+import { useState } from "react"
+import Header from "../components/Header"
+import DataTable from "../components/DataTable"
+import Searcher from "../components/Searcher"
 
 
 export default function Home() {
 
-    const [receivedData, setReceivedData] = useState({})
-    const [userInfo, setUserInfo] = useState({})
-    const [consents, setConsents] = useState([])
-    const [policy, setPolicy] = useState({})
-    const [visible, setVisible] = useState(false)
+    const [orgRef, setOrgRef] = useState("");
+    const [data, setData] = useState([]);
+    const [available, setAvailable] = useState(false);
 
-    const collapse = () => {
-        setVisible(false)
+    const updateRef = (e) => {
+        setOrgRef(e.target.value)
     }
 
-    const expand = () => {
-        if(JSON.stringify(receivedData)=== "{}")
-            return
-        setVisible(true)
-    }
-
-    useEffect(() => {
-        window.addEventListener("message", (e) => {
-            if(e.origin === "http://localhost:3000"){
-                setReceivedData(e.data)
-            }
-        })
-    },[])
-
-    useEffect(async() => {
-        if(JSON.stringify(receivedData) === "{}") {
-            setUserInfo({})            
-            setConsents([])
-            setPolicy({})
-            setVisible(false)
-            return
+    const fetchData = async () => {
+        if(orgRef === "") {
+            alert("Please, insert your organization reference!")
+            return;
         }
-        const response = await fetch('/api/get-subject-data', {
-            method: "POST",
+        const response = await fetch('/api/get-org-consents', {
+            method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(receivedData)
+            body: JSON.stringify(orgRef)
         })
-        const data = await response.json()
-        setUserInfo(data.user)
-        setConsents(data.consents)
-        setPolicy(data.policy)
-        setVisible(true)
-
-    }, [receivedData])
-
-    const formik = useFormik({
-        initialValues: {},
-        onSubmit: async (values) => {
-            const allConsents = consents.map(c => c.id)
-            const givenConsents = values.checked.map(c => parseInt(c))
-
-            const data = {allConsents, givenConsents, userInfo}
-
-            // alert(JSON.stringify(data, null, 2))
-
-            setVisible(false) 
-
-            const response = await fetch('/api/update-subject-data', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            const responseJson = await response.json()
-            alert(JSON.stringify(responseJson, null, 2))
-        },
-    })
+        const consents = await response.json();
+        if(JSON.stringify(consents)==="{}") {
+            alert("No results were found!")
+            setAvailable(false)
+            return;
+        }
+        setAvailable(true)
+        setData(consents)
+    }
 
     return (
-        <div>
-                <div className={visible ? "main-container" : "hidden"}>
-                    <div className={visible ? "child-container left-child" : "hidden"}>
-                        <h2 className={JSON.stringify(receivedData) === "{}" ? "hidden" : "centered"}><Typography variant="h4">Consent Policy</Typography> </h2>
-                        <Typography component="p">
-                            {policy.policy}
-                        </Typography>
-                    </div>
-                    <div className="separator"></div>
-                    <div className={visible ? "child-container right-child" : "hidden"}>
-                        <form onSubmit={formik.handleSubmit} className="form">
-                            <Consent consents={consents} formik={formik} />
-                            <div className={JSON.stringify(receivedData) === "{}" ? "hidden" : "form-submit-button"}>
-                                <Button variant="contained" type="submit">Submit</Button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="close-button">
-                        <Button aria-label="Close" size="large" onClick={collapse} >
-                            <ExpandMore/>
-                        </Button>
-                    </div>
+        <>
+            <div style={{height:"12vh", position:'fixed', zIndex:3, width:'100%'}}>
+                <Header/>
+            </div>
+            <div style={{display:'flex', top:'12%', position:'absolute', height:'88vh', width:'100%'}}>
+                <div style={{flex:1, height:'100%'}}>
+                    <Searcher updateRef={updateRef} orgRef={orgRef} fetchData={fetchData}/> 
                 </div>
-                <div className={visible ? "hidden" : "open-button"}>
-                    <Button aria-label="Open" size="large" onClick={expand} sx={{borderRadius:"20%", backgroundColor:"#0084F7"}} >
-                        <LaunchIcon sx={{color: "black"}}/>
-                    </Button> 
+                <div style={{flex:3, height:'100%'}}>
+                    <DataTable data={data} available={available}/>
                 </div>
-        </div>
+            </div>
+        </>
     ) 
 }
