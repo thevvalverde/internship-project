4c00641c279d33c137c37ac4a0a20992931415a1
thevvalverde <fvalverde007@gmail.com>
2022-05-28 17:33:38 +0100

    Responsiveness change

--- prisma/components/Header.jsx
+++ prisma/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({active}) {
             <HeaderDiv>
                 <Container sx={{height:'100%', display:"relative"}} maxWidth="false" disableGutters>
                     <Paper elevation={0} sx={{backgroundColor:"primary.dark",height:"100%",borderBottom:"1px solid", borderColor:"primary.light", borderRadius:0, padding:"0 1%"}}>
-                        <Typography variant="h2" sx={{fontWeight:700,display:'inline-block', width:"25%", fontSize:55}} color="secondary">BackOffice</Typography>
+                        <Typography variant="h2" sx={{fontWeight:700,display:'inline-block', width:"25%", fontSize:55, margin:'0.5% 0 0 0.5%'}} color="secondary">BackOffice</Typography>
                         <div style={{display: 'inline-block', width:"auto",height:"100%", float:"right", paddingRight:"1%"}}>
                             <Link href="/">
                                 <Button variant="text" color={active===0 ? "info" : "secondary"} size="large"><h3>Org Searcher</h3></Button>
